22da3a7afb9fe3280a81ab20becce97d7546d638
Shashikanth Veligeti <shashikanth.veligeti@rackspace.com>
2022-02-03 12:30:55 +0530

    initial commit

---
+++ tf workspace.tf
@@ -0,0 +1,20 @@
+/*provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAUN2ZIAH4ERYW4MW4"
+  secret_key = "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"
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-06cffe063efe892ad"
+   instance_type = lookup(var.instance_type,terraform.workspace)
+  }
+
+ variable "instance_type"{
+   type = "map"
+   default = {
+  default = "t2.nano"
+  dev = "t2.micro"
+  prod = "t2.large"
+  }
+}
+*/
